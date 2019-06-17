e64db692742c41ce8cbb363c5f58ea48bc06fa77
Nikita Alexandar <alexandar.nikita@zoho.com>
2019-06-17 23:44:08 +0300

    navbar complete

--- js/common.js
+++ js/common.js
@@ -1,17 +1,9 @@
 
-function openNav() {
-  document.getElementById("myNav").style.width = "100%";
-}
-
-function closeNav() {
-  document.getElementById("myNav").style.width = "0%";
-}
-
 $(function() {
   $('.nav-tabs a').click(function(){
     $(this).tab('show');
   });
-  
+
   $('.fa-search').click(function() {
     if ($('.custom-dropdown-menu').hasClass('active')) {
       $('.custom-dropdown-menu').removeClass('active');
@@ -19,11 +11,22 @@ $(function() {
       $('.custom-dropdown-menu').addClass('active');
     }
   });
-  
+
   $('.btn-select').click(function () {
     $('.btn-select').each(function () {
       $(this).removeClass('active');
     });
     $(this).addClass('active');
+  });
+
+  $('.btn-sub-menu').click(function () {
+    $('.custom-sub-dropdown-menu').removeClass('active');
+    if ($(this).hasClass('active')) {
+      $(this).removeClass('active');
+    } else {
+      $('.btn-sub-menu').removeClass('active');
+      $(this).addClass('active');
+      $(this).next().addClass('active');
+    }
   })
 });
